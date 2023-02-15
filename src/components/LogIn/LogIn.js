5de408af8db83fca39528d8b303c7c5d8f595e9a
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-15 23:27:18 +0600

    besic setup

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div>
+      <h3>Log in here.</h3>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ005HK-vq-Pymz9zmGCg4uO-ZC4pdoLM",
+  authDomain: "router-firebase-combinat-799e4.firebaseapp.com",
+  projectId: "router-firebase-combinat-799e4",
+  storageBucket: "router-firebase-combinat-799e4.appspot.com",
+  messagingSenderId: "8101217687",
+  appId: "1:8101217687:web:56ead20b952b04e9cbb0d2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
